6d774b241064502bd65d30da874e2bbc44bca276
superbeom <it.super.beom@gmail.com>
2022-11-28 15:26:12 +0900

    Update WhatsNew

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,3 +1,17 @@
-const NewFeatures = () => <div>New Features</div>;
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex flex-col flex-1 min-w-[210px] sm:max-w-[250px]">
+    <div className="flex-center w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]">
+      <img className="w-1/2 h-1/2 object-contain" src={imgUrl} alt="icon" />
+    </div>
+
+    <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
+      {title}
+    </h1>
+
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]">
+      {subtitle}
+    </p>
+  </div>
+);
 
 export default NewFeatures;
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,5 +1,48 @@
 "use client";
 
-const WhatsNew = () => <section>What's new section</section>;
+import { motion } from "framer-motion";
+
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { newFeatures } from "../constants";
+
+import { NewFeatures, TitleText, TypingText } from "../components";
+
+const WhatsNew = () => (
+  <section className="relative paddings z-10">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="inner-width flex flex-col lg:flex-row mx-auto gap-8"
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex flex-col flex-[0.75] justify-center"
+      >
+        <TypingText title="| What's New?" />
+
+        <TitleText title={<>What's new about Metaversus?</>} />
+
+        <div className="flex flex-wrap justify-between mt-[48px] gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
+
+      <motion.div
+        variants={planetVariants("right")}
+        className="flex-center flex-1"
+      >
+        <img
+          className="w-[90%] h-[90%] object-contain"
+          src="/whats-new.png"
+          alt="whats-new"
+        />
+      </motion.div>
+    </motion.div>
+  </section>
+);
 
 export default WhatsNew;
