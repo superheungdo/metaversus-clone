e4c4a1d63c7072f0f26313cae1a40fa885b530bc
superbeom <it.super.beom@gmail.com>
2022-11-28 11:20:02 +0900

    Update About

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,5 +1,20 @@
 "use client";
 
-export const TypingText = () => <p>Typing Text</p>;
+import { motion } from "framer-motion";
 
-export const TitleText = () => <h2>Title Text</h2>;
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span key={index} variants={textVariant2}>
+        {letter === " " ? "\u00A0" : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
+
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
